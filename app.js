d160117a62427e7390bf22ebadf660fb282c09eb
humairfattani <humairfattani950@gmail.com>
2021-12-13 03:16:17 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,109 @@
+//    Question # 1
+// var a = prompt("First name")
+// var b = prompt("Last name")
+// var fullname = a+b
+// alert(fullname)
+
+
+//    Question # 2
+// var c = prompt("Enter favourite mibile phone model")
+// var d = c.length
+// alert(d)
+
+//    Question # 3
+// var e = ("Pakistan")
+// var f = e.indexOf("n")
+// alert(f)
+
+// //    Question # 4
+// var g = ("Hello World")
+// var h = g.lastIndexOf("l")
+// alert(h)
+
+//     Question # 5
+// var i = ("Pakistani")
+// var j = i.charAt("3")
+// alert(j)
+
+//     Question # 6
+// var k = prompt("First name")
+// var l = prompt("Last name")
+// var fullname = k.concat(l)
+// alert(fullname)
+
+//     Question # 7
+// var m = ("Hyderabad")
+// var n = m.replace("Hyder","islam")
+// alert(n)
+
+//     Question # 8
+// var o = ("Ali and Sami are best friends. They play cricket and football together.")
+// var p = o.replace(/and/g,"&")
+// alert(p)
+
+//     Question # 9
+// var q = ("472")
+// var r = parseInt(q)
+// console.log(r)
+
+//     Question # 10
+// var s = prompt("Enter")
+// var t = s.toUpperCase()
+// console.log(t)
+
+//      Question # 11
+//  var w = prompt("Enter")
+//  var x = w.slice(0,1)
+//  x = x.toUpperCase()
+//  var y = w.slice(1)
+//  y = y.toLowerCase()
+//  console.log(x+y)
+
+//     Question # 12
+// var z = (3536)
+// var aa = z.toString()
+// console.log(aa)
+
+//    Question # 14 
+// var arr = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var ab = prompt("Welcome to ABC Bakery.What do you want to order sir/ma'am?")
+// alert(arr.indexOf(ab))
+
+//   Question # 15
+// var input = prompt("Enter password");
+// var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for (var i = 0; i < nums.length; i++) {
+//   if (input.charAt(0) != nums[i]) {
+//     alert("password is  valid");
+//     break;
+//   } else
+//     alert("Password should not start with number");
+// }  
+
+//    Question # 16
+// var text = "university of karachi"
+// const myArray = text.split("")
+// console.log(myArray)
+
+//   Question # 17
+// var ac = prompt()
+// var ad = ac.charAt(ac.length-1)
+//   alert(ad)
+
+//  Question # 18 
+{
+
+    //int length = strlen(string);
+     var ad = prompt()
+    for (i = 0; i < 20; i++)
+    {
+        if (string[i] == ch)
+        {
+            count++;
+            alert(ad)
+        }
+    }
+
+    return count;
+}
